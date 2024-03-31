e7de2dfc0f429052f356355869f358abbcd39fae
zzb610 <bwyuchi@gmail.com>
2024-03-31 20:16:25 +0800

    show hidden files

--- plugins/user.lua
+++ plugins/user.lua
@@ -9,4 +9,13 @@ return {
   --     require("lsp_signature").setup()
   --   end,
   -- },
+  require('neo-tree').setup {
+  filesystem = {
+    filtered_items = {
+      visible = true, -- This is what you want: If you set this to `true`, all "hide" just mean "dimmed out"
+      hide_dotfiles = false,
+      hide_gitignored = false,
+      }
+    }
+  }
 }
