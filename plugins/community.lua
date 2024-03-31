1f1fddbe055ec8e8c6157f3feb5c2b0614d2a1da
zzb610 <bwyuchi@gmail.com>
2024-03-31 20:37:56 +0800

    change theme

--- plugins/community.lua
+++ plugins/community.lua
@@ -4,7 +4,7 @@ return {
   -- example of importing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
-  -- { import = "astrocommunity.colorscheme.catppuccin" },
+  { import = "astrocommunity.colorscheme.catppuccin" },
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
   { import = "astrocommunity.pack.cpp" },
   { import = "astrocommunity.pack.python" },
